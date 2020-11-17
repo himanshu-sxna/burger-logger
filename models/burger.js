4ba88f8fc03d95cd4660476667b877d4f376beb2
Himanshu Saxena <contact@hellohimanshu.com>
2020-11-17 22:10:18 +1030

    defined statements to call orm functions

---
+++ models/burger.js
@@ -0,0 +1,7 @@
+const orm = require("../config/orm");
+
+orm.selectAll("burger_name", "burgers");
+
+orm.insertBurger("burgers", "burger_name", ); //get burger name
+
+orm.updateBurger("burgers", "burger_name", ); // get burger name
